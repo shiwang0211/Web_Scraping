00b517b79cd2680b310bd6241b3f93b0dad33390
Your <shiwang0211@gmail.com>
2017-08-06 22:00:14 -0400

    Update

--- Main.py
+++ Main.py
@@ -16,13 +16,14 @@ import mysql.connector
 import re
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 # Settings
 base_review_url_us = 'https://myanimelist.net/anime/'
 base_search_url_jp = r'https://www.animesachi.com/visitor/search.php?sort=oastyle_up&key='
 base_review_url_jp = r'https://www.animesachi.com/visitor/'
 score_jp = re.compile(r'中央 ([0-9]+)点/平均')
-MAX_REVIEW = 1000
+MAX_REVIEW = 10000
 
 # Function to connect to google cloud sql
 def connect_to_cloudsql():
@@ -48,7 +49,7 @@ def getbsObj(url):
 
 # Extract rates from websites
 def getReview():
-    for index in range(1,MAX_REVIEW):
+    for index in range(1001,MAX_REVIEW):
         review_url = base_review_url_us + str(index)
         bsObj = getbsObj(review_url)
         
@@ -70,6 +71,7 @@ def getReview():
                     pass
                 
             print('Finished US ID ' + str(index) + '---')
+            conn.commit()
             
             # Extract japanese name and recode it
             Name_JP = bsObj.find(text= 'Japanese:').parent.parent.get_text()[12:-3]
@@ -88,6 +90,7 @@ def getReview():
                 c.execute("UPDATE anime SET jp_score = '%s' where id = '%s'" \
                           % (Score_JP, index))
                 print('Finished JP ID ' + str(index) + '---')
+                conn.commit()
             except:
                 pass
         else:
@@ -114,6 +117,8 @@ def TwoHist(dataset):
     plt.tight_layout()
     plt.savefig('./fig/Two_His_Plot.png',dpi=100)    
 
+def TwoCorr(dataset):
+    sns.set_style("darkgrid")
     sns.jointplot(x='us_score', y='jp_score', data=dataset).set_axis_labels('US Score','JP Score')
     plt.savefig('./fig/Two_Corr_Plot.png',dpi=100)
     
@@ -139,7 +144,7 @@ def Analyze():
     c.execute('select * from anime')
     entries = c.fetchall()
     anime_reviews = pd.DataFrame(entries)
-    
+
     c.execute('select * from anime_genre')
     entries = c.fetchall()
     anime_genres = pd.DataFrame(entries)
@@ -148,6 +153,8 @@ def Analyze():
     anime_reviews.columns = ['id','name','us_score','jp_score','rating','producer']
     anime_reviews = anime_reviews.dropna(axis=0, how='any')
     anime_reviews.us_score = anime_reviews.us_score * 10
+    anime_reviews['diff_score'] = anime_reviews.us_score - anime_reviews.jp_score 
+    top_25_producers = anime_reviews.producer.value_counts().keys()[:25]
     
     anime_genres.columns = ['id','genre']
     anime_genres = pd.merge(anime_genres, anime_reviews, left_on=['id'], right_on = ['id'],\
@@ -157,10 +164,24 @@ def Analyze():
     
     # Plots
     CountPlot(anime_reviews, 'rating', 'Rating',4,2)
-    CountPlot(anime_reviews, 'producer', 'Producer',15,1)
+    CountPlot(anime_reviews[anime_reviews.producer.isin(top_25_producers)], 'producer', 'Producer',4,2)
     TwoHist(anime_reviews)
+    TwoCorr(anime_reviews)
     ByColumnComparison(anime_reviews,'rating')
     ByColumnComparison(anime_genres,'genre')
+
+    # Summary Table
+    # 1.paired t test
+    stats.ttest_rel(anime_reviews.us_score,anime_reviews.jp_score)
+    
+    # 2.Group by rating & genre
+    summary_rating = anime_reviews.groupby('rating').agg({'diff_score': [np.mean, np.std]})
+    summary_genre = anime_genres.groupby('genre').agg({'diff_score': [np.mean, np.std]})
+    summary_producer = anime_reviews[anime_reviews.producer.isin(top_25_producers)].groupby('producer').agg({'diff_score': [np.mean, np.std]})
+        
+    #g = sns.FacetGrid(anime_genres[anime_genres.genre.isin(top_15_genres)], row='genre',
+    #                  size=1.7, aspect=4,)
+    #g.map(sns.distplot, 'diff_score', hist=True, rug=False,kde=False);
 
 
 if __name__ == '__main__':
